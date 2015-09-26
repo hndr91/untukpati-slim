8e9d9032200a55cb486599becac79b7763642c6d
Afif Hendrawan <afif.hendrawan@gmail.com>
2015-09-26 19:51:33 +0700

    db handler for insert new type and new service

--- include/config.php
+++ include/config.php
@@ -3,4 +3,13 @@ define('DB_USERNAME','root');
 define('DB_PASSWORD','');
 define('DB_HOST','localhost');
 define('DB_NAME','untukpati');
+
+define('SERVICE_TYPE_ADDED_SUCCESSFULLY', 0);
+define('SERVICE_TYPE_ADD_FAILED', 1);
+define('SERVICE_TYPE_ALREADY_EXISTED', 2);
+
+define('SERVICE_ADDED_SUCCESSFULLY', 0);
+define('SERVICE_ADD_FAILED', 1);
+define('SERVICE_ALREADY_EXISTED', 2);
+
 ?>
--- include/dbHandler.php
+++ include/dbHandler.php
@@ -110,6 +110,119 @@ class dbHandler {
     return $services;
   }
 
+  /**
+  * Insert new service type
+  * @param $typeName
+  **/
+  public function postNewServiceType($typeName) {
+    $res = array();
+
+    if(!$this->isTypeExists($typeName)) {
+      $id = ''; // set empty, because id is autoincrement
+
+      $query = "INSERT INTO service_type(id_type, type_name) VALUES (?,?)"
+
+      $stmt = $this->conn->prepare($query);
+      $stmt->bind_param('ss', $id,$typeName);
+      $result = $stmt->execute();
+      $stmt->close();
+
+      if($result) {
+        return SERVICE_TYPE_ADDED_SUCCESSFULLY;
+      } else {
+        return SERVICE_TYPE_ADD_FAILED;
+      }
+    } else {
+      return SERVICE_TYPE_ALREADY_EXISTED;
+    }
+
+    return $res;
+  }
+
+  /**
+  * Insert new service
+  * @param $id, $serviceName, $serviceAddress, $serviceTelp, $serviceEmail, $serviceType, $serviceDistrict,
+  * @param $serviceInfo, $serviceImgUrl, $serviceLocationName, $serviceLocationLong, $serviceLocationLat
+  **/
+  public function postNewServiceList($serviceName, $serviceAddress, $serviceTelp, $seviceEmail, $serviceType, $serviceDistrict,
+  $serviceInfo, $serviceImgUrl, $serviceLocationName, $serviceLocationLong, $serviceLocationLat) {
+    $res = array();
+
+    if(!$this->isServiceExists($serviceName)) {
+      $id = '';
+
+      $query = "INSERT INTO service_list(id_service, service_name, service_address, service_telp, service_email,
+        service_type, service_district, service_info, service_image_url,
+        service_location_name, service_location_long, service_location_lat)
+                VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+
+      $stmt = $this->conn->prepare($query);
+      $stmt->bind_param('ssssssssssss',
+              $id,
+              $serviceName,
+              $serviceAddress,
+              $serviceTelp,
+              $serviceEmail,
+              $serviceType,
+              $serviceDistrict,
+              $serviceInfo,
+              $serviceImgUrl,
+              $serviceLocationName,
+              $serviceLocationLong,
+              $serviceLocationLat);
+
+      $result = $stmt->execute();
+      $stmt->close();
+
+      if($result) {
+        return SERVICE_TYPE_ADDED_SUCCESSFULLY;
+      } else {
+        return SERVICE_TYPE_ADD_FAILED;
+      }
+
+    } else {
+      return SERVICE_ALREADY_EXISTED;
+    }
+
+    return $res;
+  }
+
+
+  /**
+  ============== HELPER METHOD =====================
+  **/
+
+  /**
+  * Check existing type
+  * @param $typeName
+  **/
+  public function isTypeExists($typeName) {
+    $query = "SELECT * FROM service_type WHERE type_name = ?";
+
+    $stmt = $this->conn->prepare($query);
+    $stmt->bind_param('s', $typeName);
+    $stmt->execute();
+    $stmt->store_result();
+    $rows = $stmt->rows;
+    $stmt->close();
+    return $rows > 0;
+  }
+
+  /**
+  * Check existing service
+  * @param $serviceName
+  **/
+  public function isServiceExists($serviceName) {
+    $query = "SELECT id_service, service_name FROM service_list WHERE service_name = ?";
+
+    $stmt = $this->conn->prepare($query);
+    $stmt->bind_param('s', $serviceName);
+    $stmt->execute();
+    $stmt->store_result();
+    $rows = $stmt->rows;
+    $stmt->close();
+    return $rows > 0;
+  }
 
 }
 ?>
